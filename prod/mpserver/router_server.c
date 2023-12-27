7f1c0243039ea46655d4429c5bf5a8d478f7c300
startlab <fengqianguo@tencent.com>
2023-12-27 10:43:45 +0800

    1227_003(dbg mpserver with gongfeng 0403)

--- prod/mpserver/router_server.c
+++ prod/mpserver/router_server.c
@@ -374,7 +374,9 @@ void receiveMessage(int index)
         DEBUG_LOG_D("socket index(%d) close", index);
         session_node* closeNode = get_session_node_index(index);
         if (closeNode != NULL) {
-            mp_stop_session(closeNode->session_id ,SESSION_STOPREASON_NORMAL);
+            
+            DEBUG_LOG_D("111111111111111bytesRecv is (%d)", bytesRecv);
+            //mp_stop_session(closeNode->session_id ,SESSION_STOPREASON_NORMAL);
         } else {
             closeSocketFd(msgSocket);
             removeSocket(index);            
